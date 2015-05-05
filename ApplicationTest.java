0fc40e25b4138b039671b487b0c4002a68b49926
Soraolala <tranportconnection@gmail.com>
2015-05-05 17:16:14 +0700

    Manh
    Manh

--- ApplicationTest.java
+++ ApplicationTest.java
@@ -2,8 +2,3 @@ package vn.com.bkuapp.bkuapp;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
-
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- 
-}
